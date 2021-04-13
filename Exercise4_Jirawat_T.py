94248618dfe3c1d60bfc6fc3754a57ddf677f481
caaraamelz <82438039+caaraamelz@users.noreply.github.com>
2021-04-13 14:30:49 +0700

    Add files via upload

---
+++ Exercise4_Jirawat_T.py
@@ -0,0 +1,9 @@
+FE = 80.00
+GB = 75.00
+ICS = 70.00
+ComPro = 99.99
+print("         ---Your score---")
+print("Foundation English               :",FE)
+print("General Business                 :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming             :",ComPro)
